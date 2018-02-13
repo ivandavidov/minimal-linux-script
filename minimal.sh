66b0c9590abb8a127bb2f42f1bf7bd43ceeee245
SuperDuperHacker <nagakamira@gmail.com>
2018-02-13 11:43:18 -0500

    bumb Linux kernel to 4.15.3 and busybox to 1.28.0
    busybox 1.28.0 works good

--- minimal.sh
+++ minimal.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -ex
-KERNEL_VERSION=4.12.3
-BUSYBOX_VERSION=1.27.1
+KERNEL_VERSION=4.15.3
+BUSYBOX_VERSION=1.28.0
 SYSLINUX_VERSION=6.03
 wget -O kernel.tar.xz http://kernel.org/pub/linux/kernel/v4.x/linux-$KERNEL_VERSION.tar.xz
 wget -O busybox.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
